d5c1d5f2db9c19c3a277376b1a14df821d45f6f7
tyoc213 <tyoc213+git@tyoc.mx>
2022-07-07 20:40:11 -0500

    sha1_cracker: read lines

---
+++ src/bin/sha1_cracker.rs
@@ -0,0 +1,38 @@
+use std::{
+    env,
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn usage() {
+    let s = r#"Usage:
+sha1_cracker: <wordlist.txt> <sha1_hash>"#;
+    println!("{}", s);
+}
+
+const SHA1_HEX_STRING_LENGTH: usize = 40;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        usage();
+        return Ok(());
+    }
+
+    let hash_to_crack = args[2].trim();
+    if hash_to_crack.len() != SHA1_HEX_STRING_LENGTH {
+        return Err("sha1 hash is not valid".into());
+    }
+
+    let wordlist_file = File::open(&args[1])?;
+    let reader = BufReader::new(&wordlist_file);
+
+    for line in reader.lines() {
+        let line = line?.trim().to_string();
+        println!("{}", line);
+    }
+
+    Ok(())
+}
